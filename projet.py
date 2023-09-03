08a806556090550d2896b023c56390e35c93b345
nnouali <143795535+nnouali@users.noreply.github.com>
2023-09-03 23:53:18 +0200

    Create projet.py

---
+++ projet.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+# matrice de score avec les bons para
+def score_matrix (rows, colums) :
+    matrix = [] 
+    for i in range (rows):
+        row = []
+        for j in range (colums):
+            score = 0
+            row.apped(score)
+        matrix.append()
+    return matrix
+#ou 
+matrix = np.array([
+    [0, 0, 30],
+    [50, 5, 25],
+    [8, 12, 22]
+])
