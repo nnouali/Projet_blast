c41d99e60342cababe83e963a531ad478cf53346
nnouali <noualinoura@gmail.com>
2023-09-05 17:20:19 +0200

    test
    avancée projet

--- projet.py
+++ projet.py
@@ -11,10 +11,36 @@ def score_matrix (rows, colums) :
         matrix.append()
     return matrix
 #ou 
-matrix = np.array([
+matrix_similarite = np.array([
     [0, 0, 30],
     [50, 5, 25],
     [8, 12, 22]
 ])
 
-#test pour git
+def calculate_score(query, database_sequence):
+    # Calculez le score de correspondance entre la séquence de requête et la séquence de la base de données.
+    score = 0
+    for i in range(len(query)):
+        if query[i] == database_sequence[i]:
+            score += 1
+    return score
+
+def blast(query, database, threshold):
+    results = []
+    for db_sequence in database:
+        for i in range(len(db_sequence) - len(query) + 1):
+            subsequence = db_sequence[i:i+len(query)]
+            score = calculate_score(query, subsequence)
+            if score >= threshold:
+                results.append((db_sequence, i, score))
+    return results
+
+# Exemple d'utilisation
+query_sequence = "AGTCA"
+database_sequences = ["AGTCCATGC", "AGTCACTGA", "TGGGAGTCA"]
+threshold_score = 17
+
+results = blast(query_sequence, database_sequences, threshold_score)
+
+for result in results:
+    print(f"Alignement trouvé dans la séquence : {result[0]} à la position {result[1]} avec un score de {result[2]}")
