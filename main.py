cfbddf0b38dd9e1347a72b0d5f26b6a83ad3d580
nnouali <noualinoura@gmail.com>
2023-09-13 20:12:29 +0200

    sauvegarde pour test

--- main.py
+++ main.py
@@ -114,21 +114,92 @@ with open("seq_voisins_scores.txt", 'r') as file:
                 'scores': scores
             }
 
+
 # Affichez l'alignement des mots étendus avec les scores à chaque position
 for (sequence_id, neighbor_word), data in extended_word_data.items():
     extended_word = data['extended_word']
     alignment = data['alignment']
     db_sequence = data['db_sequence']
     scores = data['scores']
+# Récupérez le mot d'origine à partir des données de chaque voisin
+    matching_neighbor = [neighbor for neighbor in matching_neighbors if neighbor[1] == neighbor_word][0]
+    original_word = matching_neighbor[2]
 
-    print(f"ID de séquence : {sequence_id}, Mot Voisin : {neighbor_word}")
+    print() 
+    print(f"ID séquence requete: {sequence_id}, Mot Voisin : {neighbor_word}")
+    print("Mot d'Origine      : ", original_word)
     print(f"Mot Étendu : {extended_word}")
     print(f"Séquence de la Base de Données : {db_sequence}")
     print("Alignement :")
-    print("Mot Étendu        : ", extended_word)
-    print("Alignement        : ", alignment)
-    print("Base de Données   : ", db_sequence)
-    print("Scores            : ", ' '.join([f"{score:.1f}" for score in scores]))
-      # Affichez l'alignement avec des traits
+    print("     Mot Étendu        : ", extended_word)
+    print("                       : ", alignment)
+    print("     Base de Données   : ", db_sequence)
+    print("     Scores            : ", ' '.join([f"{score:.1f}" for score in scores]))
 
-print("Affichage des alignements avec scores terminé.")
+    # Calculer les scores cumulés
+    cumulative_scores = []
+    cumulative_score = 0.0
+    for score in scores:
+        cumulative_score += score
+        cumulative_scores.append(cumulative_score)
+    
+    print("     Scores Cumulés    : ", ' '.join([f"{score:.1f}" for score in cumulative_scores]))
+ 
+ # Trouver l'indice k où le mot voisin correspondant à db_sequence commence
+    k = db_sequence.find(neighbor_word)
+
+    if k != -1:
+        # Calculer les scores cumulés à partir de la position k vers la droite
+        cumulative_scores_d = []
+        cumulative_score_d = 0.0
+        for l in range(k, len(scores)):
+            cumulative_score_d += scores[l]
+            cumulative_scores_d.append(cumulative_score_d)
+        
+        print("     Scores Cumulés à partir du Mot Voisin : ", ' '.join([f"{score:.1f}" for score in cumulative_scores_d]))
+    else:
+        print("     Scores Cumulés à partir du Mot Voisin : N/A (Mot Voisin introuvable)")
+
+    # Trouver l'indice i où le mot voisin correspondant à db_sequence commence
+    i = db_sequence.find(neighbor_word)
+
+    if i != -1:
+        # Commencer à cumuler à partir de la fin du mot voisin
+        start_position = i + len(neighbor_word) - 1  # Position du dernier acide aminé du mot voisin dans db_sequence
+        
+        # Calculer les scores cumulés à partir de la position start_position vers la gauche
+        cumulative_scores_g = []
+        cumulative_score_g = 0.0
+        for j in range(start_position, -1, -1):  # Commencez à partir de start_position jusqu'au début (de droite à gauche)
+            cumulative_score_g += scores[j]
+            cumulative_scores_g.insert(0, cumulative_score_g)  # Insérer au début de la liste
+            
+        print("     Scores Cumulés à partir de la Fin du Mot Voisin (de droite à gauche) : ", ' '.join([f"{score:.1f}" for score in cumulative_scores_g]))
+    else:
+        print("     Scores Cumulés à partir de la Fin du Mot Voisin (de droite à gauche) : N/A (Mot Voisin introuvable)")
+
+# ...
+
+# Calculer le maximum du Scores Cumulés à partir du Mot Voisin
+max_cumulative_score_g = max(cumulative_scores_g)
+max_cumulative_score_d = max(cumulative_scores_d)
+print("max_cumulative_score_d", max_cumulative_score_d)
+
+# Calculer les indices où les maximums des Scores Cumulés à partir du Mot Voisin sont atteints
+max_cumulative_score_index_g = cumulative_scores_g.index(max_cumulative_score_g)
+max_cumulative_score_index_d = cumulative_scores_d.index(max_cumulative_score_d)
+
+# Trouver la position à partir de laquelle l'alignement complet commence
+start_position = max_cumulative_score_index_g
+
+# Trouver la position à laquelle l'alignement complet se termine
+end_position = max_cumulative_score_index_d
+
+# Extraire l'alignement complet entre ces positions
+alignment_complete = extended_word[start_position:end_position + 1]
+
+print(alignment_complete)
+
+# Afficher l'alignement complet (jusqu'au maximum du Scores Cumulés à partir du Mot Voisin)
+print("Alignement complet jusqu'au maximum du Scores Cumulés à partir du Mot Voisin :")
+print(alignment_complete)
